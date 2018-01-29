c473c09fd73ea5f016e901ae5618a86717e53047
Lauren M Hale <lmhale710@gmail.com>
2018-01-29 16:42:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,3 +13,17 @@ var instruments =["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
 }
 return empArray;
 }
+
+function johnLennonFacts (arr) {
+var results = [];
+  var facts = ["He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"];
+
+  while (i < facts.length){
+      i++;  
+    results.push(facts[i] + '!!!');
+}
+return results;
+}
