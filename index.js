22720791f0c7d9e50fddaac7a75ef1089190ea3c
Lauren M Hale <lmhale710@gmail.com>
2018-01-29 16:16:18 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,15 @@
+var musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+var instruments =["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+
+function theBeatlesPlay(arr1,arr2){
+  var musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+var instruments =["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+  var empArray = [];
+  for(var i = 0;i < musicians.length; i++){
+     for (var k = 0; k < instruments.length; i++){
+      
+       empArray.push(musicians[i] + 'plays' + instruments[k]);
+    
+  }
+}
+}
